e7da7ee029f2497a3b175739c9c98ff63861f987
Muhammad Satria Ramadhan <muhammad.satria12@ui.ac.id>
2023-12-05 22:49:14 +0700

    Revisi template

--- reservasipenjemputan/views.py
+++ reservasipenjemputan/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
 from django.db import connection, DatabaseError
-from django.views.decorators.csrf import csrf_exempt
 
-@csrf_exempt
 def reservasipenjemputan(request):
-    return render(request, "reservasi_penjemputan,html")
+    return render(request, "reservasi_penjemputan.html")
--- sistel/urls.py
+++ sistel/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('reservasipenjemputan/', include('reservasipenjemputan.urls')),
+    path('', include('reservasipenjemputan.urls')),
 ]
