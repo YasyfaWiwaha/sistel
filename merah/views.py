8c15b9dd1a6a00e158a023a6e6b31fa032c71a78
ihzadfm <112409398+ihzadfm@users.noreply.github.com>
2023-12-12 20:04:35 +0700

    Add files via upload

--- merah/views.py
+++ merah/views.py
@@ -39,19 +39,54 @@ def dashboard(request: HttpRequest):
 
     cursor.execute(sql)
     user_data = dictfetchall(cursor)[0]
+    
+    room_data = []
+    facility_data = []
+    if is_hotel:
+        hotel_name = user_data["hotel_name"]
+        sql = f"select * from sistel.room where hotel_name = '{hotel_name}' "
+        cursor.execute(sql)
+        room_data = dictfetchall(cursor)
 
-    return render(request,"dashboard.html",{"title": "dashboard user",**user_data})
+        sql = f"select * from sistel.hotel_facilities where hotel_name = '{hotel_name}' "
+        cursor.execute(sql)
+        facility_data = dictfetchall(cursor)
+
+    return render(request,"dashboard.html",{"title": "dashboard user","room_data": room_data,"facility_data": facility_data,**user_data})
 
 def complaint(request: HttpRequest,id):
     form = ComplaintForm()
     cursor = connection.cursor()
     email = request.session["akun_pengguna"].get("email",None)
+    is_hotel = request.session["akun_pengguna"].get("is_hotel",False)
+    
+    sql = f"""
+        select *
+        from sistel.user_acc
+        inner join sistel.customer
+        on user_acc.email = customer.email
+        where user_acc.email = '{email.lower()}';
+        """
+            
+    if is_hotel:
+        sql = f"""
+        select *
+        from sistel.user_acc
+        inner join sistel.hotel
+        on user_acc.email = hotel.email
+        where user_acc.email = '{email.lower()}';
+        """
+
+    cursor.execute(sql)
+    user_data = dictfetchall(cursor)[0]
+    user_name = user_data["fname"] + " " + user_data["lname"]
     
     if request.method == "POST":
         if form.is_valid():
             hotel_name = form.cleaned_data["hotel_name"]
             hotel_chapter = form.cleaned_data["hotel_chapter"]
             description = form.cleaned_data["description"]
+ 
 
             sql =f"""
                 INSERT INTO complaint (
@@ -76,4 +111,4 @@ def complaint(request: HttpRequest,id):
 
             return redirect("dashboard")
     
-    return render(request,"complaint.html",{"title": "Complaint","form": form})
+    return render(request,"complaint.html",{"title": "Complaint","form": form,"email": email,"name": user_name})
